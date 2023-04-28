2b34980dda7ad3260d4b0b31b4fb83a7d9191013
Linus Frederik Grebing <linus.frederik.grebing@iem.thm.de>
2023-04-28 17:34:04 +0200

    Agency Template integrated

---
+++ src/Controller/OnePagerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OnePagerController extends FrontendController
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function defaultAction(Request $request): Response
+    {
+        return $this->render('onepager/onepager.html.twig');
+    }
+}
