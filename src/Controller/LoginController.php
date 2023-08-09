0057114ddaa002a6a3a6220728334ce338ad105d
Nilspaul <nilspaul27@gmail.com>
2023-08-09 16:08:34 +0200

    login and registration working via class

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,57 +2,60 @@
 
 namespace App\Controller;
 
-use Pimcore\Model\User;
+use Pimcore\Model\DataObject\User; // Import the User class from DataObject
 use Pimcore\Controller\FrontendController;
-use Pimcore\Db;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends FrontendController
-{   
+{
     /**
      * @param Request $request
      * @return Response
      */
-    
+
     public function defaultAction(Request $request): Response
     {
         return $this->render('main/main.html.twig');
     }
+
     public function login(Request $request): Response
-{
-    // Retrieve the form data
-    $email = $request->request->get('email');
-    $password = $request->request->get('password');
+    {
+        // Retrieve the form data
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
 
-    // Fetch the user by email using a custom SQL query
-    $query = "
-        SELECT * FROM users
-        WHERE email = :email
-    ";
+        // Fetch the user from the "User" folder using manual search
+        $users = new User\Listing(); // Get a listing of User objects
+        $users->setCondition("email = ?", [$email]);
+        $users->setLimit(1);
 
-    $params = [
-        'email' => $email,
-    ];
+        foreach ($users as $user) {
+            if ($user instanceof User) {
+                // Verify the password
+                if (password_verify($password, $user->getPassword())) {
+                    // Password is correct, proceed with the login logic
 
-    $user = Db::get()->fetchRow($query, $params);
+                    // Redirect the user to a protected page or perform any other actions
+                    $session = $this->get('session');
+                    $session->set('user_logged_in', true);
 
-    // Check if a user with the given email exists
-    if (!$user) {
-        // User not found, handle the error accordingly
+                    return $this->render('onepager/onepager.html.twig');
+                }
+            }
+        }
+
+        // User not found or incorrect password, handle the error accordingly
         return new Response('Invalid email or password!');
     }
-    // Verify the password
-    if (!password_verify($password, $user['password'])) {
-        // Incorrect password, handle the error accordingly
-        return new Response('Invalid email or password!');
+
+    public function logout(Request $request): Response
+    {
+        // Clear the user's session
+        $session = $this->get('session');
+        $session->remove('user_logged_in');
+
+        // Redirect to the login page
+        return $this->render('onepager/onepager.html.twig'); // Replace with the actual route name for your login page
     }
-
-    // Password is correct, proceed with the login logic
-
-    // Redirect the user to a protected page or perform any other actions
-
-    return new Response('Login successful!');
-}
-
 }
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,40 +2,47 @@
 
 namespace App\Controller;
 
-use Pimcore\Model\User;
+use Pimcore\Model\DataObject\User; // Import the User class from DataObject
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Pimcore\Model\Asset;
 
 class RegisterController
 {
     public function register(Request $request): Response
     {
         // Retrieve the form data
-        $name = $request->request->get('name');
+        $username = $request->request->get('name');
         $email = $request->request->get('email');
         $password = $request->request->get('password');
         $confirmPassword = $request->request->get('confirm_password');
-
+        
         // Validate the form data, perform any necessary checks
-
         if ($password !== $confirmPassword) {
             return new Response('Passwords do not match!');
         }
-
+        
         // Hash the password
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-        // Create a new user
+    
+        // Create a new User object (Pimcore DataObject)
         $user = new User();
-        $user->setUsername($name);
+        $user->setKey($username); // Set a unique key, typically username or email
+        $user->setParentId(7); // Set the parent folder's ID where users are stored
+        $user->setPublished(true); // Mark the user as published
+        
+        // Set user properties using setter methods
+        $user->setUsername($username);
         $user->setEmail($email);
         $user->setPassword($hashedPassword); // Set the hashed password
+
+        $image = Asset\Image::getByPath("/Team/lisa_ranold.png");
+        $user->setImage($image);
+        // Save the user object
         $user->save();
-
-        // Optionally, perform additional actions such as sending a confirmation email, etc.
-
+        
         // Redirect the user to a success page
         return new Response('User registered successfully!');
     }
+    
 }
-
