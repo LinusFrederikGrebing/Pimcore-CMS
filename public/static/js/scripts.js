7812e1c6595a61bd52ef00415492af098f552130
Linus Frederik Grebing <linus.frederik.grebing@iem.thm.de>
2023-08-29 18:55:14 +0200

    tooltip

--- public/static/js/scripts.js
+++ public/static/js/scripts.js
@@ -339,7 +339,7 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
+
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -349,10 +349,7 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+  
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
 
