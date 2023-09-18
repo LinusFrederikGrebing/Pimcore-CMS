8e5638e4d23acbddcc6d8ff94dc735021f76ba83
Nilspaul <nilspaul27@gmail.com>
2023-09-18 14:13:11 +0200

    blockRoutesMiddleware

---
+++ src/Middleware/RouteBlockingMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Middleware;
+
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;  
+
+
+class RouteBlockingMiddleware implements EventSubscriberInterface
+{
+    private $blockedRoutes;
+
+    public function __construct(array $blockedRoutes)
+    {
+        $this->blockedRoutes = $blockedRoutes;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        $pathInfo = $request->getPathInfo();
+
+        if ($this->shouldBlockRoute($pathInfo)) {
+            $event->setResponse($this->redirect('/'));
+        }
+    }
+
+    private function shouldBlockRoute($path)
+    {
+        return in_array($path, $this->blockedRoutes);
+    }
+
+    private function redirect($url)
+    {
+        return new RedirectResponse($url);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
+    }
+}
